6c3bff6bdf247495ebfb1244010ca4fd9c40200c
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-11-16 14:54:24 +0000

    neaten tests

--- test/woodbury_pd_mat.jl
+++ test/woodbury_pd_mat.jl
@@ -4,54 +4,52 @@
     S = Diagonal(randn(4).^2 .+ 1)
     x = randn(size(A, 1))
 
-    @test_throws ArgumentError WoodburyPDMat(randn(5, 2), D, S)
-    @test_throws ArgumentError WoodburyPDMat(randn(4, 3), D, S)
-    @test_throws ArgumentError WoodburyPDMat(A, Diagonal(.-randn(2).^2), S)
-    @test_throws ArgumentError WoodburyPDMat(A, D, Diagonal(.-randn(4).^2))
-
     W = WoodburyPDMat(A, D, S)
     W_dense = PDMat(Symmetric(Matrix(W)))
 
-    # Checks getindex works.
-    @test all(isapprox.(W, W_dense))
+    @testset "invalid constructors error" begin
+        @test_throws ArgumentError WoodburyPDMat(randn(5, 2), D, S)
+        @test_throws ArgumentError WoodburyPDMat(randn(4, 3), D, S)
+        @test_throws ArgumentError WoodburyPDMat(A, Diagonal(.-randn(2).^2), S)
+        @test_throws ArgumentError WoodburyPDMat(A, D, Diagonal(.-randn(4).^2))
+    end
 
-    @test PDMats.unwhiten!(similar(x), W, x) ≈ PDMats.unwhiten!(similar(x), W_dense, x)
+    @testset "Basic functionality" begin
+        # Checks getindex works.
+        @test all(isapprox.(W, W_dense))
+    end
+
+    @testset "unwhiten!" begin
+        @test PDMats.unwhiten!(similar(x), W, x) ≈ PDMats.unwhiten!(similar(x), W_dense, x)
+    end
 
     @testset "logdet" begin
-
         @test logdet(W) ≈ logdet(W_dense)
-
-        test_function = (A, D, S) -> logdet(WoodburyPDMat(A, D, S))
-
-        test_ad(test_function, randn(), A, D, S)
+        test_ad(randn(), A, D, S) do A, D, S)
+            logdet(WoodburyPDMat(A, D, S))
+        end
     end
 
     @testset "invquad" begin
         @test invquad(W, x) ≈ invquad(W_dense, x)
 
-        test_function = (A, D, S, x) -> begin
+        test_ad(randn(), A, D, S, x) do A, D, S, x
             W = WoodburyPDMat(A, D, S)
             return HeavyTailedPredictors.invquad(W, x)
         end
-
-        test_ad(test_function, randn(), A, D, S, x)
     end
 
     @testset "MvNormal logpdf" begin
-
         m = randn(size(A, 1))
         @test logpdf(MvNormal(m, W), x) ≈ logpdf(MvNormal(m, Symmetric(Matrix(W))), x)
 
-        test_function = (m, A, D, S, x) -> begin
+        test_ad(randn(), m, A, D, S, x) do m, A, D, S, x
             W = WoodburyPDMat(A, D, S)
             return logpdf(MvNormal(m, W), x)
         end
-
-        test_ad(test_function, randn(), m, A, D, S, x)
     end
 
     @testset "GenericMvTDist logpdf" begin
-
         α = 2.1
         m = randn(size(A, 1))
         @test isapprox(
@@ -59,11 +57,9 @@
             logpdf(Distributions.GenericMvTDist(α, m, W_dense), x),
         )
 
-        test_function = (α, m, A, D, S, x) -> begin
+        test_ad(randn(), α, m, A, D, S, x) do α, m, A, D, S, x
             W = WoodburyPDMat(A, D, S)
             return logpdf(Distributions.GenericMvTDist(α, m, W, false), x)
         end
-
-        test_ad(test_function, randn(), α, m, A, D, S, x)
     end
 end
