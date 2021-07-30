b6e94f828eb9fa4632b05230be589f30a8efcbe5
Alex Robson <alex.robson@invenialabs.co.uk>
2021-07-30 11:23:14 +0100

    Remove merge mess

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,18 +1,5 @@
 @non_differentiable validate_woodbury_arguments(A, D, S)
 
-function _times_pullback(Ȳ::AbstractMatrix, A, B, proj)
-    #Ā = @thunk(proj[:A](dot(Ȳ, B)'))
-    #B̄ = @thunk(proj[:B](A' * Ȳ))
-    Ā = dot(Ȳ, B)'
-    B̄ = A' * Ȳ
-    return (NoTangent(), Ā, B̄)
-end
-_times_pullback(ȳ::AbstractThunk, A, B, proj) = _times_pullback(unthunk(ȳ), A, B, proj)
-function _times_pullback(Ȳ::Tangent{<:WoodburyPDMat}, A, B, proj)
-    W = WoodburyPDMat(Ȳ.A, Ȳ.D, Ȳ.S)
-    return _times_pullback(W, A, B, proj)
-end
-
 function ChainRulesCore.rrule(::typeof(*), A::Real, B::WoodburyPDMat)
     project_A = ProjectTo(A)
     project_B = ProjectTo(B)
