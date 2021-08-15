dde2b469c7dc97a987fa6c427c550c69ae5a0923
Alex Robson <alex.robson@invenialabs.co.uk>
2021-08-15 11:32:15 +0100

    Rework chainrules tests. Add constructor rrule

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,10 +1,32 @@
 @non_differentiable validate_woodbury_arguments(A, D, S)
 
-function ChainRulesCore.rrule(::typeof(*), A::Real, B::WoodburyPDMat)
+# Rule for Woodbury * Real. 
+# Ignoring Complex version for now. 
+
+function ChainRulesCore.rrule(::typeof(*), A::WoodburyPDMat, B::Real)
     project_A = ProjectTo(A)
     project_B = ProjectTo(B)
     times_pullback(ȳ) = _times_pullback(ȳ, A, B, (;A=project_A, B=project_B))
     return A * B, times_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(*), A::Real, B::WoodburyPDMat, )
+    project_A = ProjectTo(A)
+    project_B = ProjectTo(B)
+    times_pullback(ȳ) = _times_pullback(ȳ, A, B, (;A=project_A, B=project_B))
+    return A * B, times_pullback
+end
+
+function _times_pullback(Ȳ::Tangent{T}, A::T, B::Real, proj) where {T<:WoodburyPDMat}
+    Ā = Tangent{WoodburyPDMat}(; A = Ȳ.A, D = Ȳ.D * B', S = Ȳ.S * B')
+    B̄ = dot(Ȳ.D, A.D) + dot(Ȳ.S, A.S)
+    return (NoTangent(), Ā, B̄)
+end
+
+function _times_pullback(Ȳ::Tangent{T}, A::Real, B::T, proj) where {T<:WoodburyPDMat} 
+    Ā = dot(Ȳ.D, B.D) + dot(Ȳ.S, B.S)
+    B̄ = Tangent{WoodburyPDMat}(; A = Ȳ.A, D = Ȳ.D * A, S = Ȳ.S * A)
+    return (NoTangent(), Ā, B̄)
 end
 
 function _times_pullback(Ȳ::AbstractMatrix, A, B, proj)
@@ -14,14 +36,26 @@ function _times_pullback(Ȳ::AbstractMatrix, A, B, proj)
 end
 _times_pullback(ȳ::AbstractThunk, A, B, proj) = _times_pullback(unthunk(ȳ), A, B, proj)
 
+# Composite pullbacks
+function ChainRulesCore.rrule(
+    ::Type{T},
+    A::AbstractMatrix,
+    D::Diagonal,
+    S::Diagonal,
+    ) where  {T<:WoodburyPDMat}
+    return WoodburyPDMat(A, D, S), X̄ -> WoodburyPDMat_pullback(X̄, A, D, S)
+end
+WoodburyPDMat_pullback(X̄::Tangent, A, D, S) = (X̄ = (unthunk(X̄); return (NoTangent(), X̄.A, X̄.D, X̄.S)))
+WoodburyPDMat_pullback(X̄::AbstractThunk, A, D, S) = WoodburyPDMat_pullback(unthunk(X̄), A, D, S)
+
 function ChainRulesCore.ProjectTo(W::T) where {T<:WoodburyPDMat}
     fields = (A = W.A, D = W.D, S = W.S)
-    ProjectTo{T}(; fields...)
+    ChainRulesCore.ProjectTo{T}(; fields...)
 end
 
-function (W::ProjectTo{T})(W̄) where {T<:WoodburyPDMat}
-    Ā(W̄) = ProjectTo(W.A)((W̄ + W̄') * (W.A * W.D))
-    D̄(W̄) = ProjectTo(W.D)(W.A' * (W̄) * W.A)
-    S̄(W̄) = ProjectTo(W.S)(W̄)
-    return Tangent{T}(; A = Ā(W̄), D = D̄(W̄), S = S̄(W̄))
+function (project::ProjectTo{T})(X̄) where {T<:WoodburyPDMat}
+    Ā = ProjectTo(project.A)((X̄ + X̄') * (project.A * project.D))
+    D̄ = ProjectTo(project.D)(project.A' * (X̄) * project.A)
+    S̄ = ProjectTo(project.S)(X̄)
+    return Tangent{WoodburyPDMat}(; A = Ā, D = D̄, S = S̄)
 end
--- test/chainrules.jl
+++ test/chainrules.jl
@@ -1,43 +1,13 @@
-# Create a struct to hold the fields of the Woodbury. 
-# We do this because the Woodbury cannot represent it's own tangent. 
-# I.e. for Y = f(W,...), W̄ = ∂Y / ∂W, is not necessarily a valid Woodbury. 
-# Consider, e.g. the case of a Positive Diagonal matrix
-struct WoodburyLike
-    A
-    D
-    S
-end
-
-# Overwrite the generic to_bec and replace with the almost identical Woodbury specific.
-# This means that in FiniteDifferences, the WoodburyLike matrix is created instead of the Woodbury. 
-# Because the construction is forced there, this would bypass the valdidation checks on the constructor.  
-function FiniteDifferences.to_vec(x::T) where {T<:WoodburyPDMat}
-    val_vecs_and_backs = map(name -> to_vec(getfield(x, name)), fieldnames(T))
-    vals = first.(val_vecs_and_backs)
-    backs = last.(val_vecs_and_backs)
-
-    v, vals_from_vec = to_vec(vals)
-    function structtype_from_vec(v::Vector{<:Real})
-        val_vecs = vals_from_vec(v)
-        values = map((b, v) -> b(v), backs, val_vecs)
-        WoodburyLike(values...)
-    end
-    return v, structtype_from_vec
-end
-
-# Assign some algebra for the WoodburyLike. 
-WoodburyPDMat(S::WoodburyLike) = WoodburyPDMat(S.A, S.D, S.S)
-Base.:*(A::AbstractVecOrMat, B::WoodburyLike) = A * WoodburyPDMat(B)
-Base.:*(A::WoodburyLike, B::AbstractVecOrMat) = WoodburyPDMat(A) * B
-Base.:*(A::Real, B::WoodburyLike) = A * WoodburyPDMat(B)
-Base.:*(A::WoodburyLike, B::Real) = WoodburyPDMat(A) * B
-LinearAlgebra.dot(A, B::WoodburyLike) = dot(A, WoodburyPDMat(B))
-
 @testset "ChainRules" begin
 
     W = WoodburyPDMat(rand(4,2), Diagonal(rand(2,)), Diagonal(rand(4,)))
     R = 2.0
     D = Diagonal(rand(4,))
+
+    @testset "Constructors" begin
+        test_rrule(WoodburyPDMat, W.A, W.D, W.S)
+        test_rrule(WoodburyPDMat, W.A, W.D, W.S; output_tangent=Tangent{WoodburyPDMat}(; A = rand(4,2), D = Diagonal(rand(2,)), S = Diagonal(rand(4,))))
+    end
 
     @testset "*(Matrix-Woodbury)" begin
         test_rrule(*, D, W)
@@ -45,53 +15,9 @@ LinearAlgebra.dot(A, B::WoodburyLike) = dot(A, WoodburyPDMat(B))
         test_rrule(*, rand(4,4), W)
     end
 
-    @testset "*(Real, Woodbury)" begin
-        @testset "Matrix Tangent" begin
-            ###
-
-            primal = R * W
-            # Matrix Tangent
-            T = rand_tangent(Matrix(primal))
-            res = ChainRulesCore.rrule(*, R, W)
-            f_jvp = j′vp(ChainRulesTestUtils._fdm, x -> Matrix(*(x...)), T, (R, W))[1]
-
-            # Expected
-            R̄ = ProjectTo(R)(dot(T, W'))
-            W̄ = ProjectTo(W)(conj(R) * T)
-
-            R̄_rrule = unthunk(res[2](T)[2])
-            W̄_rrule = unthunk(res[2](T)[3])
-
-            @test res[1] == primal
-            @test R̄_rrule ≈ f_jvp[1]
-            @test W̄_rrule.A ≈ f_jvp[2].A
-            @test W̄_rrule.D ≈ f_jvp[2].D
-            @test W̄_rrule.S ≈ f_jvp[2].S
-
-            # Cannot get this to work. Here the T will be 
-            # T = rand_tangent(primal::WoodburyPDMat) which breaks. 
-            # test_rrule(*, 5.0, W)
-
-            #####################################################################################################################
-
-            primal = R * W
-
-            # Generate the Tangent as ChainRulesTestUtils would do
-            ∂primal = rand_tangent(Random.GLOBAL_RNG, collect(primal))
-            T = ProjectTo(primal)(∂primal)
-      
-            f_jvp = j′vp(ChainRulesTestUtils._fdm, x -> (*(x...)), T, (R, W))[1]
-
-            # Expected
-            R̄ = ProjectTo(R)(dot(∂primal, W'))
-            W̄ = ProjectTo(W)(conj(R) * ∂primal)
-
-            @test res[1] == primal
-            @test R̄ ≈ f_jvp[1]
-            @test W̄.A ≈ f_jvp[2].A
-            @test W̄.D ≈ f_jvp[2].D
-            @test W̄.S ≈ f_jvp[2].S
-
-        end
+    @testset "*(Woodbury-Real)" begin
+        test_rrule(*, W, R)
+        test_rrule(*, R, W)
     end
+
 end
